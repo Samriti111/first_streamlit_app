a61ece5c44d046640153fff717abb804b1a66bf4
Samriti Manna <118884632+Samriti111@users.noreply.github.com>
2022-11-24 18:11:58 +0530

    Update streamlit_app.py
    added emoji and avocado option

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
-streamlit.title('Breakfast Menu')
-streamlit.text('Kale,Spinach & Rocket smoothie')
-streamlit.text('Hard-boiled free range eggs')
+streamlit.title(' My parents new healthy diner')
+streamlit.title('Breakfast Favourites')
+streamlit.text('🥣Omega-3 and blueberry oatmeal')
+streamlit.text('🥬Kale,Spinach & Rocket smoothie')
+streamlit.text('🐔Hard-boiled free range eggs')
+streamlit.text('🥑🍞Avocado Toast')
