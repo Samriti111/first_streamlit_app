85389090beabe89bce993826410d906e758b22a8
Samriti Manna <118884632+Samriti111@users.noreply.github.com>
2022-11-24 18:14:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title(' My parents new healthy diner')
+streamlit.header(' My parents new healthy diner')
 streamlit.title('Breakfast Favourites')
 streamlit.text('🥣Omega-3 and blueberry oatmeal')
 streamlit.text('🥬Kale,Spinach & Rocket smoothie')
 streamlit.text('🐔Hard-boiled free range eggs')
 streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🍐Build your own fruit smoothie🍑🥝')
