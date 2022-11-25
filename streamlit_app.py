a7429147f8997cd1ce6e9c73c9a6b3b88ae45811
Samriti Manna <118884632+Samriti111@users.noreply.github.com>
2022-11-25 10:07:43 +0530

    Update streamlit_app.py
    importing pandas and storing in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥬Kale,Spinach & Rocket smoothie')
 streamlit.text('🐔Hard-boiled free range eggs')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🍐Build your own fruit smoothie🍑🥝')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
